c8027a5797677e69461a73aa4eadbe6131581afa
Atabask <87390618+Atabask@users.noreply.github.com>
2022-03-09 13:44:51 +0300

    Ввод

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
-<template>
+  <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <header>
+        <div class="title">My personal costs</div>
+      </header>
+      <main>
+        <AddPaymentForm @addNewPayment="addData" ref="addpaymentForm"/>
+        <PaymentDisplay :list="paymentsList"/>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PaymentDisplay from "./components/PaymentDisplay.vue";
+import AddPaymentForm from "./components/AddPaymentForm.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PaymentDisplay,
+    AddPaymentForm
+  },
+
+    data() {
+    return {
+      paymentsList: []
+    };
+  },
+  methods: {
+    fetchData () {
+      return [
+        {
+          date: '28.03.2020',
+          category: 'Food',
+          value: 169,
+        },
+        {
+          date: '24.03.2020',
+          category: 'Transport',
+          value: 360,
+          
+        },
+        {
+          date: '24.03.2020',
+          category: 'Food',
+          value: 532,
+        
+        },
+      ]
+    },
+    addData(data){
+      this.paymentsList.push(data)
+    }
+  },
+  created() {
+    this.paymentsList = this.fetchData()
+  },
+};
 </script>
 
-<style>
+<style lang='scss' scoped>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.title {
+  font-size: 30px;
 }
 </style>
---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input placeholder="Date" v-model="value" />
+    <input placeholder="Type" v-model="category" />
+    <input placeholder="Amount" v-model="date" />
+    <button @click="onSave">Save!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddPaymentForm",
+  data() {
+    return {
+      value: "",
+      category: "",
+      date: "",
+    };
+  },
+  computed: {
+    getCurrentDate(){
+      const today = new Date()
+      const d = today.getDate()
+      const m = today.getMonth() + 1
+      const y = today.getFullYear()
+
+      return `${d}.${m}.${y}`
+    }
+  },
+  methods: {
+    onSave(){
+        const data = {
+          data: this.data || this.getCurrentDate,
+          category: this.category,
+          value: this.value 
+        }
+        this.$emit('addNewPayment', data)
+    }
+  },
+};
+</script>
+
+<style></style>
